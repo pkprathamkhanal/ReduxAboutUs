141cea0bdfac6726010ef2c3ba7ecde4323d83c0
Pratham Khanal <pkprathamkhanal@gmail.com>
2026-03-16 06:50:03 -0500

    Update vite.config.js
    Deploying the site live

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import path from 'path'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  logLevel: 'error',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  base: '/ReduxAboutUs/',
 })
