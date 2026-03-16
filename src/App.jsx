268440a16c18c694749ddd2a3d31b83298b22802
Pratham Khanal <pkprathamkhanal@gmail.com>
2026-03-16 07:49:33 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,51 @@
-import { Toaster } from "@/components/ui/toaster"
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClientInstance } from '@/lib/query-client'
-import { pagesConfig } from './pages.config'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import PageNotFound from './lib/PageNotFound'
+import { Toaster } from "@/components/ui/toaster";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClientInstance } from "@/lib/query-client";
+import { pagesConfig } from "./pages.config";
+import { Route, Routes } from "react-router-dom";
 
-const { Pages, Layout, mainPage } = pagesConfig
-const mainPageKey = mainPage ?? Object.keys(Pages)[0]
-const MainPage = mainPageKey ? Pages[mainPageKey] : <></>
+const { Pages, Layout, mainPage } = pagesConfig;
 
-const LayoutWrapper = ({ children, currentPageName }) =>
-  Layout ? (
-    <Layout currentPageName={currentPageName}>{children}</Layout>
-  ) : (
-    <>{children}</>
-  )
+const mainPageKey = mainPage ?? Object.keys(Pages)[0];
+const MainPage = mainPageKey ? Pages[mainPageKey] : () => <>No pages found</>;
 
-function App() {
+function LayoutWrapper({ children, currentPageName }) {
+  if (Layout) {
+    return <Layout currentPageName={currentPageName}>{children}</Layout>;
+  }
+
+  return <>{children}</>;
+}
+
+export default function App() {
   return (
     <QueryClientProvider client={queryClientInstance}>
-      <Router>
-        <Routes>
+      <Toaster />
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <LayoutWrapper currentPageName={mainPageKey}>
+              <MainPage />
+            </LayoutWrapper>
+          }
+        />
+
+        {Object.entries(Pages).map(([path, Page]) => (
           <Route
-            path="/"
+            key={path}
+            path={`/${path}`}
             element={
-              <LayoutWrapper currentPageName={mainPageKey}>
-                <MainPage />
+              <LayoutWrapper currentPageName={path}>
+                <Page />
               </LayoutWrapper>
             }
           />
-          {Object.entries(Pages).map(([path, Page]) => (
-            <Route
-              key={path}
-              path={`/${path}`}
-              element={
-                <LayoutWrapper currentPageName={path}>
-                  <Page />
-                </LayoutWrapper>
-              }
-            />
-          ))}
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </Router>
-      <Toaster />
-    </QueryClientProvider>
-  )
-}
+        ))}
 
-export default App
+        <Route path="*" element={<div style={{ padding: "2rem", color: "white" }}>Page not found</div>} />
+      </Routes>
+    </QueryClientProvider>
+  );
+}
